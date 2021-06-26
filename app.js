4bb747b3a99a350991cd8b256263fe9cec4fef0e
hassanarifbinarif <46750893+hassanarifbinarif@users.noreply.github.com>
2021-06-26 17:09:55 +0500

    changes

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function (err, req, res, next) {
 
 
 mongoose
-  .connect("mongodb+srv://hassan:<123>@cluster0.avb5d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  .connect("mongodb://localhost/productscrud", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
